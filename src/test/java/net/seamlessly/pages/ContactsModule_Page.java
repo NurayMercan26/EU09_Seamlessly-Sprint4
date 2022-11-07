eeee426b89f34cf65d4f23f148e4ec5c29fed494
refikturk <rfkturk43@gmail.com>
2022-11-07 18:18:55 +0300

    Some corrections made

---
+++ src/test/java/net/seamlessly/pages/ContactsModule_Page.java
@@ -0,0 +1,6 @@
+package net.seamlessly.pages;
+
+public class ContactsModule_Page extends BasePage{
+
+
+}
--- src/test/java/net/seamlessly/step_definitions/ContactsModule_StepDefinitions.java
+++ src/test/java/net/seamlessly/step_definitions/ContactsModule_StepDefinitions.java
@@ -1,12 +1,14 @@
 package net.seamlessly.step_definitions;
 
 import io.cucumber.java.en.When;
+import net.seamlessly.pages.ContactsModule_Page;
 
 public class ContactsModule_StepDefinitions {
 
+    ContactsModule_Page contactsModule_page = new ContactsModule_Page();
 
     @When("User clicks on {string} module")
     public void userClicksOnModule(String moduleName) {
-
+        contactsModule_page.navigateToModule(moduleName);
     }
 }
