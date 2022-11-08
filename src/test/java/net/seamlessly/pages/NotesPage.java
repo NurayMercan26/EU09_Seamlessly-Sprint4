e92793550d9b65ec0c6d7486c70eb08caebc1b32
Arife <arife4231@hotmail.com>
2022-11-08 10:00:36 +0300

    note page commit 1

---
+++ src/test/java/net/seamlessly/pages/NotesPage.java
@@ -0,0 +1,18 @@
+package net.seamlessly.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NotesPage extends BasePage{
+
+@FindBy(xpath = "//*[@id='appmenu']/li[7]/a")
+    public WebElement pencilIcon;
+
+@FindBy(id = "notes_new_note")
+    public WebElement newNoteModule;
+
+@FindBy(xpath = "//div[@class='CodeMirror-scroll']")
+    public WebElement noteWritingPage;
+
+
+}
