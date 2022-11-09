09fbd5e6ee75392ef76ca9d1396f5db3ae7cc43e
NurayMercan26 <nuraymercan26@gmail.com>
2022-11-09 19:15:24 +0300

    First Profile setting change3

--- src/test/java/net/seamlessly/runners/CukesRunner.java
+++ src/test/java/net/seamlessly/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features",
         glue = "net/seamlessly/step_definitions",
         dryRun = false,
-        tags = "@SEAMLES-2502",
+        tags = "@SEAMLES-2505",
         publish = true
 
 
