86728da62fc16ade4afe8f9828811f71654fac48
NurayMercan26 <nuraymercan26@gmail.com>
2022-11-11 21:09:27 +0300

    First Profile setting change8

--- src/test/java/net/seamlessly/pages/ProfilePage.java
+++ src/test/java/net/seamlessly/pages/ProfilePage.java
@@ -6,6 +6,35 @@ import org.openqa.selenium.support.FindBy;
 public class ProfilePage extends BasePage{
 
 
+    @FindBy(id ="user")
+    public WebElement inputUsername;
+
+    @FindBy(id ="password")
+    public WebElement inputPassword;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+    @FindBy(xpath= "//*[@id=\"expand\"]/div/img")
+    public WebElement dropdownButton;
+
+    @FindBy(xpath= "//img [@src = '/apps/settings/img/admin.svg?v=c77e0e80']")
+    public WebElement SettingButton;
+    @FindBy(xpath= "//div[@class='profile__header']")
+    public WebElement Access;
+    @FindBy(xpath= "//a[@class='primary profile__header__container__edit-button']")
+    public WebElement Edit;
+
+
+    @FindBy(xpath= "//a[@aria-label='Change privacy level of profile picture']")
+    public WebElement ProfileButton;
+
+    @FindBy(xpath= "//span[.='Synchronize to trusted servers and the global and public address book']")
+    public WebElement publicButton;
+    @FindBy(xpath= "//*[@id=\"expanddiv\"]/ul/li[5]/a\t")
+    public WebElement logout;
+
+
 
     @FindBy(id="displayname")
     public WebElement fullName;
